6aff2fcc0925a644beb7cbb8953dd9ac10cc3a32
connerum <98507011+connerum@users.noreply.github.com>
2022-09-26 03:02:55 -0400

    Class Edits

--- src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
@@ -2,43 +2,29 @@ package edu.bsu.cs222;
 
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.net.URLConnection;
-////Based on Code by Professor Gestwicki
+
 public class WikipediaRevisionReader {
-    public static void main (String[]args) throws IOException {
+    public static void main(String[] args) throws IOException {
         WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
-        Scanner scanner= new Scanner(System.in);
-        String line = scanner.nextLine();
-        String timestamp = revisionReader.getLatestRevisionOf(line);
-        System.out.println(timestamp);
-    }
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
 
-    public String getLatestRevisionOf(String articleTitle)throws IOException {
-        String urlString= String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=%s&rvprop=timestamp&rvlimit=1");
-        String encodedUrlString = URLEncoder.encode(urlString, Charset.defaultCharset());
-        try{
-            URL url = new URL(encodedUrlString);
-            URLConnection URLConnection = url.openConnection();
-            URLConnection.setRequestProperty("User-Agent","edu.bsu.cs222.WikipediaRevisionReader/0.1(landen.finlinson@bsu.edu)");
-            InputStream inputStream = URLConnection.getInputStream();
-            WikipediaRevisionParser parser = new WikipediaRevisionParser();
-            String timestamp= parser.parse(inputStream);
-            return timestamp;
+        RevisionParser revisionParser = new RevisionParser();
 
-        }catch (IOException malformedURLException){
-            throw new RuntimeException(malformedURLException);
-        }
-    }
 
-    public String read(InputStream testDataStream) throws IOException {
-            JSONArray result= (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
-            return result.get(0).toString();
     }
 }

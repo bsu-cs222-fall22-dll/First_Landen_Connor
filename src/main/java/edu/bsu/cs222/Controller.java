17c051188d7c8121bcb40f1880b625331bbc25b6
connerum <98507011+connerum@users.noreply.github.com>
2022-10-05 15:18:44 -0400

    Controller Class Changes

--- src/main/java/edu/bsu/cs222/Controller.java
+++ src/main/java/edu/bsu/cs222/Controller.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class Controller {
     @FXML
     private Label title_lb;
@@ -22,6 +26,30 @@ public class Controller {
     private TextField title_tb;
     @FXML
     private TextArea results_ta;
-    public void onButtonClick(ActionEvent event) {
+    public void onButtonClick(ActionEvent event) throws IOException {
+        userinput_lb.setText(title_tb.getText());
+        String userInput = title_tb.getText();
+
+        RevisionParser revisionParser = new RevisionParser();
+
+        InputStream inputStream0 = revisionParser.encodedUrl(userInput);
+        Redirects redirects = new Redirects();
+        System.out.println(redirects.checkRedirect(inputStream0));
+
+        InputStream inputStream = revisionParser.encodedUrl(userInput);
+        ArrayList<String> usersList = revisionParser.parseUsers(inputStream);
+
+        InputStream inputStream1 = revisionParser.encodedUrl(userInput);
+        ArrayList<String> timestampsList = revisionParser.parseTimestamp(inputStream1);
+
+        Formatter formatter = new Formatter();
+        formatter.formatted(usersList, timestampsList);
+        int count = 0;
+        for (String user : usersList) {
+            String Line = user + "\t\t" + timestampsList.get(count);
+            results_ta.appendText(Line);
+            results_ta.appendText("\n");
+            count += 1;
+        }
     }
 }
